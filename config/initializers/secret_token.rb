e7f38fe20b89d6b5ec6a51e4e4f9fa3d9d7f6fc4
MakerSquare Student <students@themakersquare.com>
2013-12-07 12:04:00 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talentcrops::Application.config.secret_key_base = '4997c1ca89de788d6110e6cf07e285236818c2680c5d845bd18f9b3cc2c3cf3d9afb7bb0064494112cb06bbdcb9655bdb5cd7f8e83c49a97e23dc1ab00886092'
