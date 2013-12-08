5852fc535f78f39189610486d92136d5c472f515
Jorge Polanco <dr.jorgepolanco@gmail.com>
2013-12-07 22:24:45 -0600

    edited views for posting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	validates :title, :user_id, :picture_url, :description, :category, :subcategory, presence: true
-	has_many :votes, through :challenge	
+	validates :title, :picture_url, :description, :category, presence: true
+	has_many :votes, through: :challenge	
 end
